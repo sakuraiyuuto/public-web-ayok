3dc7a7e254dc724a7060b8ba3cdcf4ef384c051c
mitranom <64766822+mitranom@users.noreply.github.com>
2020-06-19 07:19:28 +0700

    Add files via upload

--- web/footer.php
+++ web/footer.php
@@ -1,6 +1,46 @@
 <div class="footer">
-© 2020, Kelompok 1 PPL Informatika<br>
-Universitas Tanjungpura. All rights reserved. 
+    <!--info soal terkait ayok-->
+<div class ="info">
+    <div class="info_box">
+        <div class="category">
+            <h2>Category</h2>
+            <a href="" class="">Desain</a>
+            <a href="" class="">Kesehatan</a>
+            <a href="" class="">Reparasi</a>
+            <a href="" class="">Desain</a>
+            <a href="" class="">Kesehatan</a>
+            <a href="" class="">Reparasi</a>
+            <a href="" class="">Desain</a>
+            <a href="" class="">Kesehatan</a>
+            <a href="" class="">Reparasi</a>
+        </div>
+        <div class="info_blok1">
+        <div class="about">
+            <h2>About Us</h2>
+            <a href="" class="">Tentang AYOK</a>
+            <a href="" class="">AYOK Verified Users</a>
+            <a href="" class="">Bergabung Menjadi Servicer</a>
+        </div><div class="contact">
+        <br>
+        <h2> Contact Us </h2>
+        <p> Email Us : ayok@bisnis.com <br>
+            Contact Our CS : <br>
+            +6281349609866
+        </p>
+        </div>
+        </div>
+</div>
+        <div class='illust'>
+            <img src ="../illustration ayok.png">
+        </div>
+    </div>
+
+<div class="bottom">
+<img src="../white.png">
+<p>© 2020, Kelompok 1 PPL Informatika
+Universitas Tanjungpura. All rights reserved. </p>
+</div>
+
 </div>
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -7,17 +7,21 @@
 <html>
 <head>
 	<title>Ayo Kerja !</title>
+	<script src="https://kit.fontawesome.com/8c5a0c8768.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="sistem/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+		<!--navigation bar-->
 	<div class="topnav" id="myTopnav">
+	<img src="gambar/logo/ayok.png">
 		<a href="akun/profil.php"><b>Profil Saya</b></a>
-		<a href="sistem/logout.php" style="background-color:rgb(230,60,40);"><b>Log Out</b></a>
+		<a href="sistem/logout.php"><b>Log Out</b></a>
 		<div class="box1">
-			<form name=form1 method=post action=akun/carijasa.php onsubmit="return validate()">
-				<input class=input type=text name=cari maxlength=100 placeholder="Cari jasa disini"style="width:80%;height:30px;border-radius:30px;padding-left:10px;border:none;">
+			<form name=form1 method=post action=carijasa.php onsubmit="return validate()">
+				<input class=input type=text name=cari maxlength=100 placeholder="Cari jasa disini"style="width:80%;height:30px;border-radius:30px;padding-left:10px;border:1px solid gray;">
 				<input type=submit style="width:40px;height:30px;border-radius:10px;border:none;cursor:pointer;border:none;background-color:white;margin-left:1px;" value="Cari">
 			</form>
 		</div>
@@ -25,6 +29,7 @@
 			<i class="fa fa-bars"></i>
 		</a>
 	</div>
+	
 
 	<script type="text/javascript">
 		function validate()
@@ -61,7 +66,7 @@
 		}
 	</script>
 
-
+	
 	<div class="block1">
 		<div class="font1">
 			<h1>Hot Offers !</h1>
@@ -74,6 +79,7 @@
 					Cek rekomendasi ! >>
 				</div>
 			</a>
+	</div>
 		</div>
 		
 		<div class="block2">
@@ -83,14 +89,20 @@
 			<!--Reparasi-->
 				<a href="jasa/elektronik.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/elektronik.png"><br>		
+					<div class='jasa_icon'>
+					<i class="fas fa-charging-station"></i> 
+					</div>
+					<br>
 						Elektronik
+					
 					</div>
 				</a>
 				
 				<a href="jasa/bengkel.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/bengkel.png"><br>		
+					<div class='jasa_icon'>
+					<i class="fas fa-tools"></i> 
+					</div><br>		
 						Bengkel
 					</div>
 				</a>
@@ -98,14 +110,18 @@
 			<!--konstruksi-->
 				<a href ="jasa/arsitek.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/arsitek.png"><br>		 
+					<div class='jasa_icon'>
+					<i class="fas fa-home"></i> 
+					</div><br>		 
 						Arsitek
 					</div>
 				</a>
 			
 				<a href ="jasa/tukang.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/tukang.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-hammer"></i> 
+					</div><br>	
 						Tukang
 					</div>
 				</a>
@@ -113,21 +129,27 @@
 			<!--desain-->
 				<a href ="jasa/logoposterbanner.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/logoposterbanner.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-object-ungroup"></i> 
+					</div><br>	
 						Logo, Poster dan Banner
 					</div>
 				</a>
 		
 				<a href ="jasa/pakaian.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/pakaian.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-tshirt"></i> 
+					</div><br>	
 						Pakaian
 					</div>
 				</a>
 				
 				<a href ="jasa/lukisan.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/lukisan.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-palette"></i> 
+					</div><br>	
 						Lukisan
 					</div>
 				</a>
@@ -135,21 +157,27 @@
 			<!--kesehatan-->
 				<a href ="jasa/dokter.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/dokter.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-heartbeat"></i> 
+					</div><br>	
 						Dokter
 					</div>
 				</a>
 				
 				<a href ="jasa/pijat.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/pijat.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-sign-language"></i> 
+					</div><br>	
 						Pijat
 					</div>
 				</a>
 				
 				<a href ="jasa/terapi.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/terapi.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-spa"></i> 
+					</div><br>	
 						Terapi
 					</div>
 				</a>
@@ -157,28 +185,36 @@
 			<!--rumah tangga-->
 				<a href ="jasa/asistenrumahtangga.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/asistenrumahtangga.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-broom"></i> 
+					</div><br>	
 						Asisten Rumah Tangga
 					</div>
 				</a>
 				
 				<a href ="jasa/tukangkebun.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/tukangkebun.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-tree"></i> 
+					</div><br>	
 						Tukang Kebun
 					</div>
 				</a>
 				
 				<a href ="jasa/pengasuhbayidananak.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/pengasuhbayidananak.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-child"></i> 
+					</div><br>	
 						Pengasuh Bayi dan Anak
 					</div>
 				</a>
 				
 				<a href ="jasa/angkutanpindahrumah.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/angkutanpindahrumah.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-people-carry"></i> 
+					</div><br>	
 						Angkutan Pindah Rumah
 					</div>
 				</a>
@@ -186,28 +222,36 @@
 			<!--penampilan-->
 				<a href ="jasa/perawatantubuh.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/perawatantubuh.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-hot-tub"></i> 
+					</div><br>	
 						Perawatan Tubuh
 					</div>
 				</a>
 				
 				<a href ="jasa/salon.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/salon.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-female"></i> 
+					</div><br>	
 						Salon
 					</div>
 				</a>
 				
 				<a href ="jasa/pangkasrambut.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/pangkasrambut.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-scissors"></i> 
+					</div><br>	
 						Pangkas Rambut
 					</div>
 				</a>
 				
 				<a href ="jasa/makeupacara.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/makeupacara.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-"></i> 
+					</div><br>	
 						Make Up Acara
 					</div>
 				</a>
@@ -215,14 +259,18 @@
 			<!--kebersihan-->	
 				<a href ="jasa/layanankebersihanrumah.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/layanankebersihanrumah.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-pump-soap"></i> 
+					</div><br>	
 						Layanan Kebersihan Rumah
 					</div>
 				</a>
 				
 				<a href ="jasa/cucikendaraan.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/cucikendaraan.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-soap"></i> 
+					</div><br>	
 						Cuci Kendaraan
 					</div>
 				</a>
@@ -230,7 +278,9 @@
 			<!--transportasi-->
 				<a href ="jasa/supir.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/supir.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-car"></i> 
+					</div><br>	
 						Supir
 					</div>
 				</a>
@@ -238,14 +288,18 @@
 			<!--Layanan IT-->
 				<a href ="jasa/appsistemweb.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/appsistemweb.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-laptop-house"></i> 
+					</div><br>	
 						App, Sistem dan Web
 					</div>
 				</a>
 				
 				<a href ="jasa/dataoriented.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/dataoriented.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-database"></i> 
+					</div><br>	
 						Data Oriented
 					</div>
 				</a>
@@ -253,14 +307,20 @@
 			<!--Layanan IT-->
 				<a href ="jasa/gurules.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/gurules.png"><br>	
+					<div class='jasa_icon'>
+					<div class='jasa_icon'>
+					<i class="fas fa-chalkboard-teacher"></i> 
+					</div></i> 
+					</div><br>	
 						Guru Les
 					</div>
 				</a>
 				
 				<a href ="jasa/personaltrainer.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/personaltrainer.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-dumbbell"></i> 
+					</div><br>	
 						Personal Trainer
 					</div>
 				</a>
@@ -268,7 +328,9 @@
 			
 				<a href ="jasa/semuajasa.php">
 					<div class="box7">
-						<img src="gambar/kategori_jasa/semuajasa.png"><br>	
+					<div class='jasa_icon'>
+					<i class="fas fa-plus"></i> 
+					</div><br>	
 						Lihat Semua Jasa
 					</div>
 				</a>
